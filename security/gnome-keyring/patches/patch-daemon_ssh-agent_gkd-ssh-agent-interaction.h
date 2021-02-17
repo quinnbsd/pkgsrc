8db27f45e2ab69535549b763e66b14316f121ad5
Andrew Pamment <apamment@yandex.com>
2021-02-17 12:23:30 +1000

    update gcr fix libvolume_id attempt to fix gnome-keyring, but failed

---
+++ security/gnome-keyring/patches/patch-daemon_ssh-agent_gkd-ssh-agent-interaction.h
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- daemon/ssh-agent/gkd-ssh-agent-interaction.h.orig	2018-06-25 04:15:03.000000000 +0000
++++ daemon/ssh-agent/gkd-ssh-agent-interaction.h
+@@ -20,7 +20,7 @@
+  * Author: Daiki Ueno
+  */
+ 
+-#ifndef _GKD_SSH_AGENT_INTERACTION_H__
++#ifndef __GKD_SSH_AGENT_INTERACTION_H__
+ #define __GKD_SSH_AGENT_INTERACTION_H__
+ 
+ #include <gio/gio.h>
